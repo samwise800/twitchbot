66e3c0fd631cc71e93baa3f2bc0720f425f09c4d
samwise800 <samwise800@gmail.com>
2017-02-16 00:04:42 +0000

    Add files via upload

---
+++ cfg.py
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+HOST = "irc.twitch.tv"
+PORT = 6667
+PASS = "oauth:xxx"
+NAME = "siegegamblebot"
+CHAN = "ice_poseidon"
+OWNERS = ["samwise800"]
+UNIT = "slaves"
+PEOPLE_PER_RAID = 3
+RECRUIT_TIME = 30
+RAID_TIME = 40
+RAID_REWARD_MULT = 2
+DELAY = 1.5
+=======
+HOST = "irc.twitch.tv"
+PORT = 6667
+PASS = "oauth:xxx"
+NAME = "siegegamblebot"
+CHAN = "ice_poseidon"
+OWNERS = ["samwise800"]
+UNIT = "slaves"
+PEOPLE_PER_RAID = 3
+RECRUIT_TIME = 30
+RAID_TIME = 40
+RAID_REWARD_MULT = 2
+DELAY = 1.5
+>>>>>>> origin/master
+STARTING_MONEY = 100
